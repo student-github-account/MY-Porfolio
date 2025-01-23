630f4af2139dd1850be872dee237f550764bc8c2
Md Fahim <mdf17548@gmail.com>
2025-01-23 14:44:21 +0600

    a

--- src/pages/homepage/HomePage.jsx
+++ src/pages/homepage/HomePage.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import Banner from '../../component/banner/Banner';
-import Skill from '../../component/skills/Skill';
-import Service from '../../component/Service/Service';
-import Work from '../../component/work/Work';
-import About from '../../component/about/About';
-import Image from '../../component/backgroundimage/Image';
+import Banner from '../../component/banner/Banner.jsx';
+import Skill from '../../component/skills/Skill.jsx';
+import Service from '../../component/Service/Service.jsx';
+import Work from '../../component/work/Work.jsx';
+import About from '../../component/about/About.jsx';
+import Image from '../../component/backgroundimage/Image.jsx';
 
 const HomePage = () => {
     return (
